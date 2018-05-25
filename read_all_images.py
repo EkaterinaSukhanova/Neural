99b981ae759acda1546c6b9c9ec27dbadb862fd9
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-05-25 23:14:12 +0300

    read circles and images, teached

--- read_all_images.py
+++ read_all_images.py
@@ -8,7 +8,8 @@ def read_images(shape_name: str, predict_val: int) -> []:
     for k in range(0, 3):
         file_name = "image_test/{}{}.png"
         new_filename = file_name.format(shape_name, k + 1)
-        image = imread(new_filename, as_grey=True)
+        image_not_norm = imread(new_filename, as_grey=True)
+        image = image_not_norm / 255
         image_and_predict = (np.reshape(image, image.shape[0] * image.shape[1]), predict_val)
         all_images.append(image_and_predict)
 
